8b5481f7fdc3b28d40280ee8633e8c7ee257fec6
ichedayugo-ship-it <ichedayugo@gmail.com>
2026-04-07 21:27:38 +0900

    create day4

---
+++ python/day4.py
@@ -0,0 +1,34 @@
+def check_age(age):
+    if age > 18:
+        print("Adult")
+    else:
+        print("Minor")  
+
+check_age(53)  
+
+def check_number(number):
+    if number >0:
+        print("Positive")
+    elif number ==0:
+        print("Zero")
+    else: 
+        print("Negative")
+
+check_number(4)
+check_number(0)
+check_number(-3)
+
+def bye():
+    print("Bye")
+
+bye()
+
+def show_country(country):
+    print("Country", country)
+
+show_country("Japan")
+
+def show_number(number):
+    print("Number", number)
+
+show_number(42)    
