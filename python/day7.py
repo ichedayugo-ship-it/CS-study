37d4e6e1d1c5733342a32e0834de5700fc3e3987
ichedayugo-ship-it <ichedayugo@gmail.com>
2026-04-09 14:51:05 +0900

    Create day7.py

---
+++ python/day7.py
@@ -0,0 +1,24 @@
+colors =["red", "blue","green"]
+print(colors[1])
+
+numbers = [5,10,15]
+print(numbers[2])
+
+fruits = ["apple", "banana", "orange"]
+for fruit in fruits:
+    print(fruit)
+
+animals = ["dog", "cat"]
+animals.append("rabbit")
+print(animals)
+
+numbers = [1,2,3,4,5,6]
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+
+scores =[5,12,8,20,15]
+for score in scores:
+    if score >10:
+        print(score)
+        
