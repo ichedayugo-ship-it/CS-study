9ad9487948ed7ac37deaf1c67dd792c4b430e9e1
ichedayugo-ship-it <ichedayugo@gmail.com>
2026-04-09 13:51:03 +0900

    create day6.py

---
+++ python/day6.py
@@ -0,0 +1,28 @@
+name = input("Your name: ")
+print("hello", name)
+
+name = input("your name ")
+print("Hello", name)
+
+age = int(input("Your age"))
+print("Your age in next year", age + 1)
+
+i = check_number = int(input("Enter a number"))
+if i >0:
+    print("positive")
+elif i == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+number = int(input("Enter a number"))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")   
+
+age = int(input("Enter your age"))
+if age>18:
+    print("Adult")
+else:
+    print("Minor")
